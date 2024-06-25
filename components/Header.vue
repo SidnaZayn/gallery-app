a94bf6cc1d981db8d4fc532454519c188b8ecf75
SidnaZayn <sidnazayn11@gmail.com>
2024-06-25 12:26:59 +0700

    update bg

--- components/Header.vue
+++ components/Header.vue
@@ -1,16 +1,22 @@
 <template>
   <div
-    class="w-10/12 rounded-xl shadow-2xl h-[10vh] bg-white dark:bg-slate-800 fixed top-8 left-1/2 translate-x-[-50%] z-[100]"
-    :class="isMobile ? 'hidden' : 'block'"
+    class="w-10/12 rounded-xl shadow-2xl h-[10vh] bg-white dark:bg-slate-800 transition-all duration-1000 fixed top-8 left-1/2 translate-x-[-50%] z-[100]"
+    :class="[isMobile ? 'hidden' : 'block',{'bg-opacity-25 dark:bg-opacity-25': y > height - 80}]"
   >
     <div class="grid grid-cols-3 justify-between items-center h-full px-4">
       <div class="">
         <h1 class="font-yeseva text-5xl tracking-widest">G</h1>
       </div>
       <div class="flex justify-center items-center gap-4">
-        <NuxtLink class="uppercase font-josefin sm:font-sm" active-class="font-bold" to="/">home</NuxtLink>
-        <NuxtLink class="uppercase font-josefin sm:font-sm" active-class="font-bold" to="/albums">album</NuxtLink>
-        <NuxtLink class="uppercase font-josefin sm:font-sm" active-class="font-bold" to="/about">about</NuxtLink>
+        <NuxtLink class="uppercase font-josefin sm:font-sm" active-class="font-bold" to="/"
+          >home</NuxtLink
+        >
+        <NuxtLink class="uppercase font-josefin sm:font-sm" active-class="font-bold" to="/albums"
+          >album</NuxtLink
+        >
+        <NuxtLink class="uppercase font-josefin sm:font-sm" active-class="font-bold" to="/about"
+          >about</NuxtLink
+        >
       </div>
       <!-- dark mode toggle -->
       <div class="flex justify-end">
@@ -24,9 +30,17 @@
 </template>
 
 <script setup>
-import { useWindowSize } from "@vueuse/core";
+import { useWindowSize, useWindowScroll } from "@vueuse/core";
 
-const { width } = useWindowSize();
+const { width, height } = useWindowSize();
+const { y } = useWindowScroll();
 
 const isMobile = computed(() => width.value < 768);
+const isScrolled = computed(() => y.value > height.value);
+
+// watchEffect(() => {
+//   if (isScrolled.value) {
+//     console.log("scrolled");
+//   }
+// })
 </script>
